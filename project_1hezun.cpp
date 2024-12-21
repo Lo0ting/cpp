10bb488a00fafbd875f6d174ddb2df9d7d1db43c
Lo0ting <ivangez47@gmail.com>
2024-12-21 11:10:26 +0200

    Update project_1hezun.cpp
    додана бібліотека для коректної роботи за межами Visual Studio

--- project_1hezun.cpp
+++ project_1hezun.cpp
@@ -1,5 +1,5 @@
-
-#include <iostream>
+#include <iostream>  
+#include <algorithm>//<==== фікс проблеми *min_element(yValues.begin() тобто потрібна бібліотека
 #include <cmath>
 #include <string>
 #include <vector>
